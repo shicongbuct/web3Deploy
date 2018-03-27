ac1858c8d68796e9686fdde17bb1f4cfdf80c088
ylkjcjq <691424873@qq.com>
2018-03-27 19:40:35 +0800

    8108

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/nodes/token/contract/deploy', ControllerWeb3deploy.web3deploy);//导入 keystory
 app.post('/nodes/token/contract/addlistener', ControllerListener.addListener);//导入 keystory
-var port = process.env.PORT || 10013;
+var port = process.env.PORT || 8108;
 app.listen(port);
 console.log(`listen the port: ${port}`);
 ControllerListener.startListener();
